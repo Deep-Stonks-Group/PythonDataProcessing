75696616a259fc8fc9603dc728131ff0f435d54f
Caleb Heinzman <caleb.heinzman@gmail.com>
2021-06-29 16:28:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     install_requires = [
+         'numpy==1.19.4',
          'Historic-Crypto==0.1.4',
          'yfinance==0.1.59',
          'TA-Lib==0.4.20',
