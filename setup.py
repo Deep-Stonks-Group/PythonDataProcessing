344ec3dc310107f1362177b0cf4fa0505c487951
Caleb Heinzman <caleb.heinzman@gmail.com>
2021-06-29 16:04:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,14 @@ setup(
     name = 'forecast-team-1',
     packages = find_packages(),
     include_package_data = True,
-    dependency_links=[
-        "git+git://github.abc.com/abc/SomePrivateLib.git#egg=SomePrivateLib",
+    install_requires = [
+         'numpy==1.19.4',
+         'torch==1.8.1',
+         'torchvision==0.9.1',
+         'Historic-Crypto==0.1.4',
+         'yfinance==0.1.59',
+         'TA-Lib==0.4.20',
+         'sklearn==0.0',
+         'matplotlib==3.3.3'
     ]
 )
