7839237c160cc9567219fc02335940ba55c22f6e
Caleb Heinzman <caleb.heinzman@gmail.com>
2021-07-22 18:53:49 -0500

    Created LSTM data handle which can perform all data prepartion for LSTMs. Still need to comment.

--- PythonDataProcessing/DataRetrieval.py
+++ PythonDataProcessing/DataRetrieval.py
@@ -8,22 +8,50 @@ from pandas.core.frame import DataFrame
 
 pd.options.mode.chained_assignment = None
 
-'''
-All data goes from earliest to latest and is stored in a data frame.
-You can access individual values by doing: data['High'], data['Low']...
-'''
-
-class Scaler():
-    def __init__(self,input_features,label_features):
+class LSTM_DATA_HANDLE():
+    def __init__(self, ticker, **kwargs):
         self.data_scaler = MinMaxScaler()
         self.label_scaler = MinMaxScaler()
-        self.input_features = input_features
-        self.label_features = label_features
 
-    def set_train_size(self,train_size):
+        # Data/Feature Shape
+        self.ticker = ticker
+        self.input_features = kwargs.get('input_features', ['High', 'Low', 'Close', 'Volume', 'EMA'])
+        self.label_features = kwargs.get('label_features', ['SMA'])
+        self.indicator_key = kwargs.get('indicator_key', 'Close')
+        self.seq_length = kwargs.get('seq_length', 10)
+        self.interval = kwargs.get('interval', '1d')
+        self.period = kwargs.get('period', '2y')
+        self.start_date = kwargs.get('start_date', None)
+        self.end_date = kwargs.get('end_date', None)
+        self.training_set_coeff = kwargs.get('training_set_coeff', 0.8)
+        self.is_crpyto = kwargs.get('is_crpyto', False)
+
+    def set_train_size(self, train_size):
         self.train_size = train_size
 
-    def fit_scalers(self,data_source):
+    def retrieve_data(self):
+        '''
+        This function uses DataRtrieval to load the data, normalize it and format it to be used by the LSTM.
+        :return x: The input data for the model after it is loaded, split and ready to be used.
+        :return y: The labels for the model after it is loaded and adjusted.
+        '''
+        if not self.is_crpyto:
+            data = get_stock_data(self.ticker, interval=self.interval,
+                                  period=self.period, start_date=self.start_date, end_date=self.end_date)
+        else:
+            data = get_crypto_data(self.ticker, interval=self.interval, start_date=self.start_date,
+                                   end_date=self.end_date)
+
+        data = add_features(self.input_features, self.label_features, data, key=self.indicator_key)
+
+        self.train_size = int(len(data) * self.training_set_coeff)
+        self.set_train_size(self.train_size)
+        self.fit_scalers(data)
+        scaled_data, scaled_labels = self.normalize_stock_data(data)
+        x, y = sliding_windows(scaled_data, scaled_labels, self.seq_length)
+        return x, y
+
+    def fit_scalers(self, data_source):
         '''
         Creates the data and label scalers that are used to normalize data. This is created on the training dataset.
         :param data_source: The training dataset used to create the normalization models.
@@ -39,8 +67,6 @@ class Scaler():
         scaled_data = self.data_scaler.transform(data_source[self.input_features])
         scaled_lbls = self.label_scaler.transform(data_source[self.label_features])
         return scaled_data, scaled_lbls
-
-
 
 
 def get_crypto_data(symbol, **kwargs):
@@ -79,6 +105,11 @@ def get_crypto_data(symbol, **kwargs):
     return data
 
 
+'''
+All data goes from earliest to latest and is stored in a data frame.
+You can access individual values by doing: data['High'], data['Low']...
+'''
+
 def get_stock_data(symbol, **kwargs):
     '''
     This functions takes a symbol for a stock and returns it's historic data.
@@ -111,20 +142,22 @@ def get_stock_data(symbol, **kwargs):
 
     return stock.history(interval=interval, start=start_date, end=end_date)
 
-def add_features(input_features,label_features,data,key='Close'):
+
+def add_features(input_features, label_features, data, key='Close'):
     new_features = []
     new_features.extend(set(input_features) - set(data.columns))
     new_features.extend(set(label_features) - set(data.columns))
 
     for new_feature in new_features:
         try:
-            data = add_technical_indicators[new_feature](data,key)
+            data = add_technical_indicators[new_feature](data, key)
         except KeyError:
             print(f'No function exists to add the dimension {new_feature}')
             raise
     return data
 
-def add_SMA(data,key='Close'):
+
+def add_SMA(data, key='Close'):
     '''
     This functions takes a data frame of historic data and adds the SMA to it.
 
@@ -144,7 +177,7 @@ def add_SMA(data,key='Close'):
     return data
 
 
-def add_EMA(data,key='Close'):
+def add_EMA(data, key='Close'):
     '''
     This functions takes a data frame of historic data and adds the EMA to it.
 
@@ -163,15 +196,17 @@ def add_EMA(data,key='Close'):
     data = data.iloc[29:, :]
     return data
 
-def sliding_windows( data, labels, seq_length):
+
+def sliding_windows(data, labels, seq_length):
     x = []
     y = []
-    for i in range(data.shape[0]-seq_length-1):
-        _x = data[i: (i+seq_length)]
-        _y = labels[i+seq_length]
+    for i in range(data.shape[0] - seq_length - 1):
+        _x = data[i: (i + seq_length)]
+        _y = labels[i + seq_length]
         x.append(_x)
         y.append(_y)
     return x, y
+
 
 add_technical_indicators = {
     'EMA': add_EMA,
